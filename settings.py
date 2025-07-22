6d9dc77bac278ff5c03921d82e64b7c639019890
shayleydon <shayleydon>
2025-07-22 17:11:24 +0100

    complete

---
+++ settings.py
@@ -0,0 +1,17 @@
+# Postgre SQL
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'EW4qBUgAoDlOPmiFE8i4eW8H',
+        'HOST': '172.21.231.47',
+        'PORT': '5432',
+    }
+}
+
+INSTALLED_APPS = (
+    'standalone',
+)
+
+SECRET_KEY = 'SECRET KEY for this Django Project'
